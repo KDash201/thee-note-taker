8e895368655c6890d1231c59a99896c62ddbaac9
KDash201 <KVonMadison@Yahoo.com>
2022-01-15 13:57:37 -0500

    Updated htmlRoutes paths by removing develop root thoughout app

--- routes/apiRoutes/notesRender.js
+++ routes/apiRoutes/notesRender.js
@@ -4,14 +4,34 @@ const {
     findById,
     createNotes,
     validateNotes,
-} = require("../../lib/notes")
-
-module.exports = router;
+} = require("../../lib/notes");
+const { notes } = require("../../db/db.json")
 
 router.get("/notes", (req, res) => {
-    let results = notesData;
+    let results = notes;
     if (req.query) {
-        results = filterByQuery(req.query, notesData);
+        results = filterByQuery(req.query, results);
       }
-      res.json(notesData);
+      res.json(results);
     });
+
+    router.get("/notes/:id", (req, res) => {
+        const result = findById(req.params.id, notes);
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(404);
+        }
+      });
+
+      router.post("/notes", (req, res) => {
+        // set id based on what the next index of the array will be
+        req.body.id = notes.length.toString();
+      
+        if (!validateNote(req.body)) {
+          const note = createNewNote(req.body, notes);
+          res.json(note);
+        }
+      });
+
+      module.exports = router;
